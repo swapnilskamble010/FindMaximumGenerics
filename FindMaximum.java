4c966bf160e9192dcdbc3cc8c1f920da1be1e278
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-04-06 23:42:43 +0530

    Refactor1: all 3 methods are converted to one generic method

--- FindMaximum.java
+++ FindMaximum.java
@@ -2,33 +2,14 @@ package findmaximumgeneric;
 
 public class FindMaximum {
 	
-	public void findMaxInt(Integer num1, Integer num2, Integer num3) {
-		Integer max = num1;
-		if(num2.compareTo(max) > 0)
-			max = num2;
-		if(num3.compareTo(max) > 0)
-			max = num3;
+	public <T extends Comparable<T>> void findMax(T x, T y, T z) {
+		T max = x;
+		if(y.compareTo(max) > 0)
+			max = y;
+		if(z.compareTo(max) > 0)
+			max = z;
 		
-		System.out.println("The Maximum Integer Number is: " + max);
+		System.out.println("The Maximum is: " + max);
 	}
 
-	public void findMaxFloat(Float num1, Float num2, Float num3) {
-		Float max = num1;
-		if(num2.compareTo(max) > 0)
-			max = num2;
-		if(num3.compareTo(max) > 0)
-			max = num3;
-		
-		System.out.println("The Maximum Float Number is: " + max);
-	}
-	
-	public void findMaxString(String str1, String str2, String str3) {
-		String max = str1;
-		if(str2.compareTo(max) > 0)
-			max = str2;
-		if(str3.compareTo(max) > 0)
-			max = str3;
-		
-		System.out.println("The Maximum String is: " + max);
-	}
 }
