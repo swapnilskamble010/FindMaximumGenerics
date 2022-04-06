e5de96fc580a506bfa44ce73ab735003b5985c0d
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-04-07 00:35:00 +0530

    Add: UC4- Extended the findMax method to more than 3 parameters

--- FindMaximum.java
+++ FindMaximum.java
@@ -1,21 +1,24 @@
 package findmaximumgeneric;
 
 public class FindMaximum<T extends Comparable<T>> {
-	T x, y, z;
+	T x, y, z, a;
 	
-	public FindMaximum(T x, T y, T z) {
+	public FindMaximum(T x, T y, T z, T a) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
-		findMax(x, y, z);
+		this.a = a;
+		findMax(x, y, z, a);
 	}
 	
-	public <T extends Comparable<T>> void findMax(T x, T y, T z) {
+	public <T extends Comparable<T>> void findMax(T x, T y, T z, T a) {
 		T max = x;
 		if(y.compareTo(max) > 0)
 			max = y;
 		if(z.compareTo(max) > 0)
 			max = z;
+		if(a.compareTo(max) > 0)
+			max = a;
 		
 		System.out.println("The Maximum is: " + max);
 	}
--- Generics.java
+++ Generics.java
@@ -4,8 +4,8 @@ public class Generics {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generics Program");
-		FindMaximum<Integer> maxInt = new FindMaximum<Integer>(45, 33, 38);
-		FindMaximum<Float> maxFloat = new FindMaximum<Float>(45.5f, 38.84f, 48.5f);
-		FindMaximum<String> maxString = new FindMaximum<String>("Apple", "Peach", "Banana");
+		FindMaximum<Integer> maxInt = new FindMaximum<Integer>(45, 33, 38, 56);
+		FindMaximum<Float> maxFloat = new FindMaximum<Float>(45.5f, 38.84f, 48.5f, 58.8f);
+		FindMaximum<String> maxString = new FindMaximum<String>("Apple", "Peach", "Banana", "Pinapple");
 	}
 }
