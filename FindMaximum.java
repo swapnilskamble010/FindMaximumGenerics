769de040da9de9343e15cac4f939df84455a96b6
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-04-06 22:57:23 +0530

    Add: UC2- Max Float number from 3 values logic added

--- FindMaximum.java
+++ FindMaximum.java
@@ -2,21 +2,23 @@ package findmaximumgeneric;
 
 public class FindMaximum {
 	
-	private Integer num1, num2, num3, max;
-
-	public FindMaximum(Integer num1, Integer num2, Integer num3) {
-		this.num1 = num1;
-		this.num2 = num2;
-		this.num3 = num3;
-	}
-	
-	public void findMax() {
+	public void findMaxInt(Integer num1, Integer num2, Integer num3) {
 		Integer max = num1;
 		if(num2.compareTo(max) > 0)
 			max = num2;
 		if(num3.compareTo(max) > 0)
 			max = num3;
 		
-		System.out.println("The Maximum Number is: " + max);
+		System.out.println("The Maximum Integer Number is: " + max);
+	}
+
+	public void findMaxFloat(Float num1, Float num2, Float num3) {
+		Float max = num1;
+		if(num2.compareTo(max) > 0)
+			max = num2;
+		if(num3.compareTo(max) > 0)
+			max = num3;
+		
+		System.out.println("The Maximum Float Number is: " + max);
 	}
 }
--- Generics.java
+++ Generics.java
@@ -4,7 +4,9 @@ public class Generics {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generics Program");
-		FindMaximum maxInt = new FindMaximum(45, 33, 38);
-		maxInt.findMax();
+		FindMaximum maxInt = new FindMaximum();
+		maxInt.findMaxInt(45, 33, 38);
+		FindMaximum maxFloat = new FindMaximum();
+		maxInt.findMaxFloat(45.5f, 38.84f, 48.5f);
 	}
 }
